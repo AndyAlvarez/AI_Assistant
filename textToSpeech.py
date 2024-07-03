833e0bec375e077e11e13a401e2d381008089a81
AndyAlvarez <andydalvarez30@gmail.com>
2024-07-02 20:44:24 -0400

    changed to googles text to speech library because py3ttsx was giving errors. try to make it sound better

--- textToSpeech.py
+++ textToSpeech.py
@@ -1,8 +1,27 @@
-import pyttsx3
+#import pyttsx3
+#import creds
+#import objc
+
+#def say(text):
+    #engine = pyttsx3.init()
+    #engine.setProperty('rate', creds.SPEAK_RATE_WPM)
+    #engine.say(text) 
+    #engine.runAndWait()
+
+from gtts import gTTS
+from pydub import AudioSegment
+from pydub.playback import play
+from pydub.effects import speedup
 import creds
+import io
 
 def say(text):
-    engine = pyttsx3.init()
-    engine.setProperty('rate', creds.SPEAK_RATE_WPM)
-    engine.say(text) 
-    engine.runAndWait()
+    tts = gTTS(text=text, lang='en', slow=False)
+    with io.BytesIO() as f:
+        tts.write_to_fp(f)
+        f.seek(0)
+        audio = AudioSegment.from_file(f, format="mp3")
+        # Adjust playback speed without changing the pitch
+        if creds.SPEAK_RATE_WPM != 1.0:
+            audio = speedup(audio, playback_speed=creds.SPEAK_RATE_WPM)
+        play(audio)
